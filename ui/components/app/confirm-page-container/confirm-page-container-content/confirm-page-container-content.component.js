bb990205dc84646e388a816c326fb6eb0246e919
Maarten Zuidhoorn <maarten@zuidhoorn.com>
2023-07-28 18:13:05 +0200

    Fix regression in transaction confirmation tabs

--- ui/components/app/confirm-page-container/confirm-page-container-content/confirm-page-container-content.component.js
+++ ui/components/app/confirm-page-container/confirm-page-container-content/confirm-page-container-content.component.js
@@ -115,7 +115,7 @@ export default class ConfirmPageContainerContent extends Component {
     } = this.props;
 
     return (
-      <Tabs>
+      <Tabs defaultActiveTabKey="details">
         <Tab
           className="confirm-page-container-content__tab"
           name={t('details')}
